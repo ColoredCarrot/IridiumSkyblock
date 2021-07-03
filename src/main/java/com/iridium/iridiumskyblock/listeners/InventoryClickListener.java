266c1bd65a92e9fe4071f22318ca0e83db1609bb
Julian K <koch@in.tum.de>
2021-07-03 17:07:38 +0200

    Fix inventory click bug

--- src/main/java/com/iridium/iridiumskyblock/listeners/InventoryClickListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/InventoryClickListener.java
@@ -4,16 +4,16 @@ import com.iridium.iridiumskyblock.gui.GUI;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 
 public class InventoryClickListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getClickedInventory() != null && event.getInventory().getHolder() != null && event.getInventory().getHolder() instanceof GUI) {
+        Inventory inv = event.getClickedInventory();
+        if (inv != null && inv.getHolder() != null && inv.getHolder() instanceof GUI) {
             event.setCancelled(true);
-            if(event.getClickedInventory() == event.getInventory()) {
-                ((GUI) event.getInventory().getHolder()).onInventoryClick(event);
-            }
+            ((GUI) inv.getHolder()).onInventoryClick(event);
         }
     }
 }
