9ab56aa78630fe299732c9e4637bb985b01a1411
Julian K <koch@in.tum.de>
2021-07-03 17:07:38 +0200

    Fix inventory click bug

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerJoinListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerJoinListener.java
@@ -1,10 +1,8 @@
 package com.iridium.iridiumskyblock.listeners;
 
-import com.iridium.iridiumcore.utils.StringUtils;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.database.User;
 import com.iridium.iridiumskyblock.utils.PlayerUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,22 +13,14 @@ public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        User user = IridiumSkyblock.getInstance().getUserManager().getUser(player);
+        User   user   = IridiumSkyblock.getInstance().getUserManager().getUser(player);
         user.setBypass(false);
 
         // Update the internal username in case of name change
         user.setName(event.getPlayer().getName());
 
         // Send their island border
-        IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(player.getLocation()).ifPresent(island ->
-                PlayerUtils.sendBorder(player, island)
-        );
-
-        if (player.isOp()) {
-            Bukkit.getScheduler().runTaskLater(IridiumSkyblock.getInstance(), () ->
-                            player.sendMessage(StringUtils.color(IridiumSkyblock.getInstance().getConfiguration().prefix + " &7Thanks for using IridiumSkyblock, if you like the plugin, consider donating at &bwww.patreon.com/Peaches_MLG"))
-                    , 5);
-        }
+        IridiumSkyblock.getInstance().getIslandManager().getIslandViaLocation(player.getLocation()).ifPresent(island -> PlayerUtils.sendBorder(player, island));
     }
 
 }
